29613eb953ce4e18aa8c1f0d0ec466efdf967a7d
Awesome <awesome_devnet@outlook.com>
2025-10-07 20:56:55 +0900

    fix

--- Casks/lazyprisma.rb
+++ Casks/lazyprisma.rb
@@ -2,12 +2,12 @@ cask "lazyprisma" do
   version "0.1.0"
 
   on_intel do
-    sha256 3d96e9c94348332ae6a1e600605b2023594f2e894bec9a381a868ee6874074c9
+    sha256 "3d96e9c94348332ae6a1e600605b2023594f2e894bec9a381a868ee6874074c9"
     url "https://github.com/DokaDev/lazyprisma/releases/download/v0.1.0/lazyprisma-v0.1.0-darwin-amd64.tar.gz"
   end
 
   on_arm do
-    sha256 080e94461beda6132f30e0a5440781b3a4540766257eec7504cd9f8c815f1ca6
+    sha256 "080e94461beda6132f30e0a5440781b3a4540766257eec7504cd9f8c815f1ca6"
     url "https://github.com/DokaDev/lazyprisma/releases/download/v0.1.0/lazyprisma-v0.1.0-darwin-arm64.tar.gz"
   end
 
