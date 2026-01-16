c6d51aaac6787c559b50a933f504d1947ecc4a69
Awesome <awesome_devnet@outlook.com>
2026-01-16 23:22:45 +0900

    update to v0.2.1

--- Casks/lazyprisma.rb
+++ Casks/lazyprisma.rb
@@ -1,15 +1,15 @@
 cask "lazyprisma" do
-  version "0.1.7"
+  version "0.2.1"
   # version "0.1.3-beta"
 
   on_intel do
-    sha256 "ed29d69cd961367d5b0f0ddac4ec60ac7ebb5d8a7c6a6ffdb050d3ba0907898c"
-    url "https://github.com/DokaDev/lazyprisma/releases/download/v0.1.7/lazyprisma-v0.2.0-darwin-amd64.tar.gz"
+    sha256 "61cddcaa89103d3113ec94abaf0f1e8c0adb6d29c2843fb1cd1cbf5545ac0e32"
+    url "https://github.com/DokaDev/lazyprisma/releases/download/v0.2.1/lazyprisma-v0.2.1-darwin-amd64.tar.gz"
   end
 
   on_arm do
-    sha256 "0d56b768c913446fa31a15728ed4725c0cdec181ecb6e5ad095f2add599f6d35"
-    url "https://github.com/DokaDev/lazyprisma/releases/download/v0.1.7/lazyprisma-v0.2.0-darwin-arm64.tar.gz"
+    sha256 "0c29a448c7d9f16a000ea4cb77d740fe3cd9c0d439b9360bdd1c2f5b7488ce10"
+    url "https://github.com/DokaDev/lazyprisma/releases/download/v0.2.1/lazyprisma-v0.2.1-darwin-arm64.tar.gz"
   end
 
   name "lazyprisma"
